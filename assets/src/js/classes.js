0d0171581b6e3f9e9c7d03df4b6eb491902b119a
dev_mojtaba <mz.dev.5858@gmail.com>
2023-01-18 17:04:57 +0330

    added a class to classes that can create element and add it to the list

--- assets/src/js/classes.js
+++ assets/src/js/classes.js
@@ -1,4 +1,5 @@
 import { inventoryBudget } from "./getUserBudget.js";
+import { dataList } from "./deletesFunctions.js";
 
 export class Budget {
     constructor(budget) {
@@ -8,6 +9,41 @@ export class Budget {
 
 export class HTML {
     insertBudget(amount) {
-        inventoryBudget.innerHTML = amount
+        inventoryBudget.innerHTML = amount;
+    }
+
+    insertToList(typeText, priceText) {
+        // access to the element variables
+        let div = document.createElement("div"), typePart = document.createElement("p"), pricePart = document.createElement("p"), cb = document.createElement("input");
+
+        // set attribute to checkbox
+        cb.setAttribute("type", "checkbox");
+
+        // add some required classes to div
+        div.classList.add("js-data");
+        div.classList.add("data");
+
+        // check if inputs null or undefined it's show an error otherwise it's work successfully
+        if (typeText === null || priceText === null && typeText === undefined || priceText === undefined && typeText === '' || priceText === '') {
+            alert("لطفا تمام مقادیر خواسته شده را پر کرده سپس دوباره امتحان کنید!")
+        } else {
+            
+            // append value of parameters to textContent and add required class to elements
+            typePart.textContent = typeText;
+            // typePart.classList.add("type-part");
+            pricePart.textContent = priceText;
+            // pricePart.classList.add("price-part");
+            
+            // we need to append childs to the div
+            div.appendChild(typePart);
+            div.appendChild(pricePart);
+            
+            // append checkbox to the price part
+            pricePart.appendChild(cb);
+    
+            // at last append div to the data list and show
+            dataList.appendChild(div);
+
+        }
     }
 }
